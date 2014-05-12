26d247810024fc3f11b7f086e222341fe1fb1d2a
kahn-aar <nicolas.martinr@gmail.com>
2014-05-12 11:50:03 +0200

    Ajout de l'agent BDD
    Désolé j'ai pas eu le courage de continuer

---
+++ src/Agents/BDDAgent.java
@@ -0,0 +1,18 @@
+package Agents;
+
+import jade.core.Agent;
+
+/**
+ * Agent gérant la base de données. Lui seul en a l'accès.
+ * 
+ * @author Nicolas
+ *
+ */
+public class BDDAgent extends Agent {
+	
+	@Override
+	public void setup() {
+		super.setup();
+		
+	}
+}
